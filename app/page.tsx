e91da39a8c4b03cd8d00893c1b69e9b5ff26376a
Zayan-Mohamed <itsm.zayan@gmail.com>
2026-06-12 23:38:30 +0530

    feat: add vercel analytics
    Signed-off-by: Zayan-Mohamed <itsm.zayan@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { AuthProvider } from "@/components/auth/auth-provider";
 import { AuraChat } from "@/components/aura/aura-chat";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
     <AuthProvider>
       <AuraChat />
+      <Analytics />
     </AuthProvider>
   );
 }
